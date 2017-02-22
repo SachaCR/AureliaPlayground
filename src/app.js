d98c2d715111207daa81766dab2e0fcd25a3f386
Sachacr <sachacr@runningheroes.com>
2017-02-22 11:19:06 +0100

    Some changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
+import { RouterConfiguration, Router } from 'aurelia-router';
+
 export class App {
   constructor() { }
 
-  configureRouter(config, router) {
+  configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
     config.title = 'My Aurelia App';
     config.map([
       { route: '', name: 'todos', moduleId: 'todos/list/todo-list' },
-      { route: 'notfound', name: 'notfound', moduleId: 'not-found/not-found' },
+      { route: 'test', name: 'test', moduleId: 'not-found/not-found' },
     ]);
 
     config.mapUnknownRoutes('not-found/not-found');
