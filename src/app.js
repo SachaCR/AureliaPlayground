8a9f1a12ff8bd30d7f3f399a3c7e7be85205a71f
Sachacr <sachacr@runningheroes.com>
2017-02-22 11:09:54 +0100

    Fix todo properties binding

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ export class App {
     config.title = 'My Aurelia App';
     config.map([
       { route: '', name: 'todos', moduleId: 'todos/list/todo-list' },
-      { route: 'notfound', name: 'notfound', moduleId: 'notFound/not-found' },
+      { route: 'notfound', name: 'notfound', moduleId: 'not-found/not-found' },
     ]);
 
     config.mapUnknownRoutes('not-found/not-found');
