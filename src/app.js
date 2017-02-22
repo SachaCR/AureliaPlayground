d32a74c7099dd5541e223752077d91b8c062713a
Sachacr <sachacr@runningheroes.com>
2017-02-22 12:13:11 +0100

    Add push state to allow / in url and no #

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import { RouterConfiguration, Router } from 'aurelia-router';
 
 export class App {
+
   constructor() { }
 
   configureRouter(config: RouterConfiguration, router: Router) {
     this.router = router;
     config.title = 'My Aurelia App';
+    config.options.pushState = true;
+    config.mapUnknownRoutes('not-found/not-found');
     config.map([
-      { route: '', name: 'todos', moduleId: 'todos/list/todo-list' },
+      { route: ['', 'home'], name: 'todos', moduleId: 'todos/list/todo-list' },
       { route: 'test', name: 'test', moduleId: 'not-found/not-found' },
     ]);
-
-    config.mapUnknownRoutes('not-found/not-found');
   }
 }
